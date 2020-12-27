269444d6e5222f9b3f8f62cb6400f339147d2e43
utshomomen <mohibulmomenutsho@yahoo.com>
2020-12-28 02:31:39 +0300

    react setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+//import logo from './logo.svg';
+//import './App.css';
+
+const App= () =>{
+  return (
+    //<div className="app"></div>
+
+    <h1>welcome</h1>
+  );
+}
+
+export default App;
